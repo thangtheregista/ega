c9cf383f1d3af96222ec242a7e4fe956ae12ec07
trungnhan_nguyen <nhannguyen6533@gmail.com>
2025-07-16 18:19:41 +0700

    create the register page

--- team1/src/App.jsx
+++ team1/src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css'
 import ShopLayout from "./layouts/ShopLayout.jsx";
 import Login from "./components/Login/Login.jsx";
+import SupplierPage from "./pages/supplier/supplier-page.jsx";
 
 function App() {
 
   return (
     <>
       <Login/>
+        <SupplierPage />
     </>
   )
 }
