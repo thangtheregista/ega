ce7abc4a9157f0b6aa278f5923e610f685f2c607
trungnhan_nguyen <nhannguyen6533@gmail.com>
2025-07-17 09:37:32 +0700

    save before pull the product-categories

--- team1/src/components/happy-summer/happy-summer.jsx
+++ team1/src/components/happy-summer/happy-summer.jsx
@@ -3,6 +3,11 @@ import React from "react";
 function HappySummer() {
     return (
         <>
+            <div className="happy-summer-container">
+                <div className="h-s-header">
+                    <h1>HAPPY SUMMER - GIẢM ĐẾN 50% 🎉</h1>
+                </div>
+            </div>
         </>
     )
 }
--- team1/src/pages/client/client.jsx
+++ team1/src/pages/client/client.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import "./client.css"
 import ShopLayout from "../../layouts/ShopLayout.jsx";
 import HappySummer from "../../components/happy-summer/happy-summer.jsx";
+import Carousels from "../../components/Carousel/Carousels.jsx";
 function Client() {
     return (
         <>
             <ShopLayout>
                 <main>
+                    <Carousels />
                     <HappySummer />
                 </main>
             </ShopLayout>
