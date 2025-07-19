c8885e676c53174849dc341d63d6353ec2ed0448
nhannguyencodegym43 <nhannguyen6533@gmail.com>
2025-07-19 11:18:35 +0700

    complete the happy-summer section

--- team1/src/components/HoverImage/hoverImage.jsx
+++ team1/src/components/HoverImage/hoverImage.jsx
@@ -1,9 +1,17 @@
-import React, {useState} from "react";
-function HoverImage({pic, pic2, picName}) {
+import React, {useEffect, useState} from "react";
+function HoverImage({pic, pic2, picName, picLink}) {
     const [isHovered, setHovered] = useState(false);
+    const [thePic, setThePic] = useState(`${pic}`);
+    useEffect(() => {
+        if (picLink) {
+            setThePic(picLink);
+        } else {
+            setThePic(pic);
+        }
+    }, [picLink, pic]);
     return (
         <>
-            <img src={isHovered ? pic2 : pic}
+            <img src={isHovered ? pic2 : thePic}
                  alt={picName}
                  onMouseEnter={() => setHovered(true)}
                  onMouseLeave={() => setHovered(false)}
--- team1/src/components/happy-summer/happy-summer.jsx
+++ team1/src/components/happy-summer/happy-summer.jsx
@@ -3,6 +3,7 @@ import React, {useEffect} from "react";
 import axios from "axios";
 import {ProgressBar} from "react-bootstrap";
 import HoverImage from "../HoverImage/hoverImage.jsx";
+import CountDownTimer from "../CountdownTimer/countdown-timer.jsx";
 function HappySummer() {
     const [sofas, setSofas] = React.useState([]);
     const fetchSofas = async () => {
@@ -16,17 +17,25 @@ function HappySummer() {
     useEffect(() => {
         fetchSofas();
     }, [])
+    const [picLinks, setPicLinks] = React.useState({});
+    const handlePicLinkClick = (sofaId, picUrl) => {
+        setPicLinks(prev => ({
+            ...prev,
+            [sofaId]: picUrl
+        }));
+    };
     return (
         <>
             <div className="happy-summer-container">
                 <div className="hs-header">
                     <h2>HAPPY SUMMER - GIẢM ĐẾN 50% <img src="https://bizweb.dktcdn.net/100/491/756/themes/956460/assets/flashsale-hot.png?1746582633520" alt="Sale-icon" className="sale-icon"/></h2>
+                    <CountDownTimer initHours={13} initMinutes={22} initSeconds={11} />
                 </div>
                 <div className="hs-body">
                     {sofas.map((sofa) => (
                         <div className="hs-item" key={sofa.id}>
                             <div className="hs-img">
-                                <HoverImage pic={sofa.pic} pic2={sofa.pic2} picName={sofa.name} />
+                                <HoverImage pic={sofa.pic} pic2={sofa.pic2} picName={sofa.name} picLink={picLinks[sofa.id] || ""} />
                                 {sofa.mark && (
                                     <div className="hs-mark">{sofa.mark}</div>
                                 )}
@@ -41,9 +50,20 @@ function HappySummer() {
                             <p style={{color: "#969696", textDecoration: "line-through"}}>{sofa.originalPrice}</p> <div className="sale-num">-50%</div>
                             </div>
                             <div className="hs-smallPics">
-                                <img src={sofa.small1} alt="Small1"/>
-                                <img src={sofa.small2} alt="Small2"/>
-                                <img src={sofa.small3} alt="Small3"/>
+                                {[sofa.small1, sofa.small2, sofa.small3].map((img, idx) => {
+                                    const selectedPic = picLinks[sofa.id] || sofa.small1;
+                                    const isSelected = selectedPic === img;
+                                    return (
+                                        <img
+                                            className="small-pics"
+                                            key={img}
+                                            src={img}
+                                            alt={`Small${idx + 1}`}
+                                            onClick={() => handlePicLinkClick(sofa.id, img)}
+                                            style={{border: isSelected ? "1.5px solid black" : "1px solid #c8c8c8"}}
+                                        />
+                                    )
+                                })}
                             </div>
                             <p className="hs-sold">{sofa.sold}</p>
                             <ProgressBar style={{height: "7px", borderRadius: "50px"}}>
