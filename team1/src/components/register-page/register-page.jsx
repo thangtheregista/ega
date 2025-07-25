e66269b18cd84c8153c1a3070b8ac84713542ff2
nhannguyencodegym43 <nhannguyen6533@gmail.com>
2025-07-25 22:09:06 +0700

    save

--- team1/src/components/register-page/register-page.jsx
+++ team1/src/components/register-page/register-page.jsx
@@ -3,6 +3,23 @@ import "./register-page.css"
 import {FaFacebookF, FaGoogle} from "react-icons/fa";
 
 function Register() {
+    const [formData, setFormData] = React.useState({
+        lastName: "",
+        firstName: "",
+        phone: "",
+        email: "",
+        password: "",
+        repeatPassword: "",
+    });
+
+    function handleChange(e) {
+        setFormData({...formData, [e.target.name]: e.target.value});
+    }
+
+    function handleValidate() {
+        const newErrors = {};
+    }
+
     return (
         <>
             <div className="register-page-container">
@@ -19,27 +36,31 @@ function Register() {
                         <strong className="register-form-title">THÔNG TIN CÁ NHÂN</strong>
                         <form className="register-form">
                             <label htmlFor="last-name" className="register-label">Họ <span>*</span></label>
-                            <input type="text" className="register-input" id="last-name" placeholder="Họ" required/>
+                            <input type="text" className="register-input" name="lastName" placeholder="Họ" required
+                                   onChange={(e) => handleChange(e.target.value)}/>
                             <label htmlFor="first-name" className="register-label">Tên <span>*</span></label>
-                            <input type="text" className="register-input" id="first-name" placeholder="Tên" required/>
+                            <input type="text" className="register-input" name="firstName" placeholder="Tên" required
+                                   onChange={(e) => handleChange(e.target.value)}/>
                             <label htmlFor="phone" className="register-label">Số điện thoại <span>*</span></label>
-                            <input type="text" className="register-input" id="phone" placeholder="Số điện thoại"
-                                   required/>
+                            <input type="text" className="register-input" name="phone" placeholder="Số điện thoại"
+                                   required onChange={(e) => handleChange(e.target.value)}/>
                             <label htmlFor="email" className="register-label">Email <span>*</span></label>
-                            <input type="text" className="register-input" id="email" placeholder="Email" required/>
+                            <input type="text" className="register-input" name="email" placeholder="Email" required
+                                   onChange={(e) => handleChange(e.target.value)}/>
                             <label htmlFor="password" className="register-label">Mật khẩu <span>*</span></label>
-                            <input type="password" className="register-input" id="password" placeholder="Mật khẩu"
-                                   required/>
+                            <input type="password" className="register-input" name="password" placeholder="Mật khẩu"
+                                   required onChange={(e) => handleChange(e.target.value)}/>
                             <label htmlFor="confirm-password" className="register-label">Xác nhận mật
                                 khẩu <span>*</span></label>
-                            <input type="password" className="register-input" id="confirm-password"
-                                   placeholder="Xác nhận mật khẩu" required/>
+                            <input type="password" className="register-input" name="repeatPassword"
+                                   placeholder="Xác nhận mật khẩu" required
+                                   onChange={(e) => handleChange(e.target.value)}/>
                             <button type="submit" className="register-button">Đăng ký</button>
                             <div className="register-social">
                                 <p className="register-social-text">Hoặc đăng ký bằng</p>
                                 <div className="register-social-buttons">
-                                    <button className="register-social-facebook"><FaFacebookF /></button>
-                                    <button className="register-social-google"><FaGoogle /></button>
+                                    <button className="register-social-facebook"><FaFacebookF/></button>
+                                    <button className="register-social-google"><FaGoogle/></button>
                                 </div>
                             </div>
                         </form>
