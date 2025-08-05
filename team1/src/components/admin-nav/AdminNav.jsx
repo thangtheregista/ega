3292ac4c78b3b8cf7f039a5e654b25596213a1e6
trungnhan_nguyen <nhannguyen6533@gmail.com>
2025-08-05 14:56:49 +0700

    save

--- team1/src/components/admin-nav/AdminNav.jsx
+++ team1/src/components/admin-nav/AdminNav.jsx
@@ -1,7 +1,6 @@
 import "./adminNav.css";
 import { Link } from "react-router-dom";
 import { useEffect, useState } from "react";
-import adminAvatar from "./image/avatar.jpg"; 
 
 export default function AdminNav() {
   const [currentUser, setCurrentUser] = useState({});
@@ -14,38 +13,30 @@ export default function AdminNav() {
   return (
     <div className="navbar">
       <div className="navbar-left">
-        <span className="hot-icon">🔥</span>
-        <span className="hot-message">
-          Something you love is now on sale!{" "}
-          <span className="buy-now">Buy now !</span>
-        </span>
+        <div className="logo">
+          <Link to="/"><img src="https://bizweb.dktcdn.net/100/491/756/themes/956460/assets/logo.png?1746582633520" alt="Logo"/></Link>
+        </div>
       </div>
 
       <div className="navbar-right">
         <div className="lang-switch">
           <img src="https://flagcdn.com/w40/vn.png" alt="VN Flag" />
-          <span>EN</span>
+          <span>VN</span>
         </div>
 
         <div className="icon-group">
-          <div className="icon-item">🟪</div>
           <div className="icon-item">🔍</div>
           <div className="icon-item">⭐</div>
-          <div className="icon-item">🥰</div>
-
-          <div className="icon-item">
-            🛒<span className="badge red">2</span>
-          </div>
           <div className="icon-item">
             🔔<span className="badge green">4</span>
           </div>
         </div>
 
         <div className="profile">
-          <img src={adminAvatar} alt="Profile" className="avatar" />
+          <img src={currentUser.avatar} alt="Profile" className="avatar" />
           <div className="profile-info">
             <span>{`${currentUser.lastName || ""} ${currentUser.firstName || ""}`}</span>
-            <span>Admin ▼</span>
+            <span>Quản trị viên ▼</span>
           </div>
         </div>
       </div>
