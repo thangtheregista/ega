86ad32b2b2aac97d7d6ce2cb32fc05bb1a63b2a0
trungnhan_nguyen <nhannguyen6533@gmail.com>
2025-08-06 21:04:34 +0700

    save

--- team1/src/components/ProductList/ProductItems.jsx
+++ team1/src/components/ProductList/ProductItems.jsx
@@ -7,17 +7,16 @@ export default function ProductItems({currentItems, handleDelete}) {
                 <tr>
                     <td><img src={product.pic}/>{product.name}</td>
                     <td>{product.category}</td>
+                    <td>{product.originalPrice}</td>
                     <td>{product.salePrice}</td>
                     <td><span className="stars">{product.rating}</span></td>
                     <td>
-                        <Link to={`/ega/dashboard/products/${product.id}`}>
+                        <Link to={`/ega/dashboard/products/edit/${product.id}`}>
                             <button className="edit-btn">Sửa</button>
                         </Link>
                     </td>
                     <td>
-                        <Link to={`/ega/dashboard/products/delete/${product.id}`}>
-                            <button className="delete-btn" onClick={() => handleDelete(product.id)}>Xoá</button>
-                        </Link>
+                        <button className="delete-btn" onClick={() => handleDelete(product.id)}>Xoá</button>
                     </td>
                 </tr>
             ))}
