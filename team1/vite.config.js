60988643dbfe9b4d3670215ee3881699ee0cbe5f
thangtheregista <thangtheregista@gmail.com>
2025-08-09 12:30:30 +0700

    deploy

--- team1/vite.config.js
+++ team1/vite.config.js
@@ -4,4 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/module4/team1/', // 👈 if you want site at username.github.io/module4/
+
 })
