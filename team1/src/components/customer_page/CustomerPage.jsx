053431e27efa078732508340dcc0d07c502ab94c
thangtheregista <thangtheregista@gmail.com>
2025-08-13 20:28:21 +0700

    Add missing CSS file

--- team1/src/components/customer_page/CustomerPage.jsx
+++ team1/src/components/customer_page/CustomerPage.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import "./customerPage.css";
+import "./CustomerPage.css"
 import axios from "axios";
 import {Link, useNavigate} from "react-router-dom";
 import {useState} from 'react';
